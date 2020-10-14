209e0399e3cae3b87bfc5d59c68631e738a5093c
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-10-14 19:53:08 +0100

    playgroundActivity ended

---
+++ app/src/main/java/com/example/mobilecodeeditor/CodeEditor.kt
@@ -0,0 +1,11 @@
+package com.example.mobilecodeeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CodeEditor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_editor)
+    }
+}
--- app/src/main/java/com/example/mobilecodeeditor/MainActivity.kt
+++ app/src/main/java/com/example/mobilecodeeditor/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mobilecodeeditor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var playGroundCode:AutoCompleteTextView
+    lateinit var runCode:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playGroundCode = findViewById(R.id.playGroundCode)
+        runCode = findViewById(R.id.runCode)
+
+        runCode.setOnClickListener {
+            executeCode()
+        }
+    }
+
+    private fun executeCode() {
+        val intent = Intent(MainActivity@this,CodeEditor::class.java)
+        intent.putExtra("code",playGroundCode.text.toString())
+        startActivity(intent)
     }
 }
