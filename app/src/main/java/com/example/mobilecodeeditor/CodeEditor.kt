a7957e8f513fd730398fb12630c47e7aa81e90ba
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-10-15 19:24:57 +0100

    web view configured

--- app/src/main/java/com/example/mobilecodeeditor/CodeEditor.kt
+++ app/src/main/java/com/example/mobilecodeeditor/CodeEditor.kt
@@ -2,10 +2,16 @@ package com.example.mobilecodeeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class CodeEditor : AppCompatActivity() {
+    lateinit var webBrowser:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_code_editor)
+
+        webBrowser = findViewById(R.id.webBrowser)
+
+        webBrowser.loadData(intent.getStringExtra("code"),"text/html; charset=utf-8",null)
     }
 }
